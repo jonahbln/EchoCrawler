f9723d78bdee030155756926d071cc82f8dddd1c
Jasonvu123 <98994631+Jasonvu123@users.noreply.github.com>
2023-11-02 11:28:24 -0400

    playercontroller works

--- EchoCrawler/Assets/Scripts/PlayerController.cs
+++ EchoCrawler/Assets/Scripts/PlayerController.cs
@@ -8,44 +8,57 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private bool isRotatingLeft = false;
     private bool isRotatingRight = false;
+    private bool canMove = true; // Initialize as true to allow initial movement
+
+    private bool moveForward = false;
+    private bool moveLeft = false;
+    private bool moveRight = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        // Get button inputs from the player
+        moveForward = Input.GetKeyDown(KeyCode.W);
+        moveLeft = Input.GetKeyDown(KeyCode.A);
+        moveRight = Input.GetKeyDown(KeyCode.D);
+    }
+
     private void FixedUpdate()
     {
-        // Get input from the player
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
         // Rotate the player based on input
-        if (horizontalInput < 0 && !isRotatingLeft)
+        if (moveLeft && !isRotatingLeft && canMove)
         {
             isRotatingLeft = true;
             isRotatingRight = false;
             transform.Rotate(0, 0, 90); // Rotate 90 degrees left (A key)
+            canMove = false; // Set canMove to false after key press
         }
-        else if (horizontalInput > 0 && !isRotatingRight)
+        else if (moveRight && !isRotatingRight && canMove)
         {
             isRotatingRight = true;
             isRotatingLeft = false;
             transform.Rotate(0, 0, -90); // Rotate 90 degrees right (D key)
+            canMove = false; // Set canMove to false after key press
         }
 
-        if (horizontalInput == 0)
+        if (!moveLeft && !moveRight)
         {
             isRotatingLeft = false;
             isRotatingRight = false;
+            canMove = true; // Reset canMove when not pressing A or D
         }
 
         // Move the player forward by a fixed tile distance when pressing W
-        if (verticalInput > 0)
+        if (moveForward && canMove)
         {
             Vector2 forwardDirection = transform.up;
             Vector2 newPosition = rb.position + forwardDirection * tileMoveDistance;
             rb.MovePosition(newPosition);
+            canMove = false; // Set canMove to false after key press
         }
     }
 }
